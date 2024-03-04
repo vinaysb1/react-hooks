d4efaae45a1d8d40886da617722308c97dfb4cda
unknown <vinaykumarbeelagi@gmail.com>
2024-03-04 12:57:03 +0530

    Updating Objects and Arrays in State

--- src/Car.js
+++ src/Car.js
@@ -8,12 +8,20 @@ const Car = () => {
         year: "1964",
         color: "red"
       });
+
+     const updateColor = () => {
+    setCar(prevState=> {
+       return {...prevState, color: "blue" }
+      });
+    }
   return (
     <div>
         <h1>My {car.brand}</h1>
       <p>
         It is a {car.color} {car.model} from {car.year}.
       </p>
+      <button type='button'
+      onClick={updateColor}>change color</button>
     </div>
   )
 }
