366497030f1ce8c24328fa0b2aae058ad844aafb
unknown <vinaykumarbeelagi@gmail.com>
2024-03-04 12:41:25 +0530

    single Hook that holds an object i usestate

--- src/Car.js
+++ src/Car.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState } from 'react';
 
-function Car() {
-    const [brand,setBrand] = useState('Ford')   
-    const [model,setModel] = useState('Mustang')
-    const [year,setYear] = useState(2018)
-    const [color,setColor] = useState('red')
+const Car = () => {
+    const [car, setCar] = useState({
+        brand: "Ford",
+        model: "Mustang",
+        year: "1964",
+        color: "red"
+      });
   return (
     <div>
-        <h1>my {brand}</h1>
-        <p>I have {brand} {color} car taken in the year {year}</p>
-        </div>
+        <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+    </div>
   )
 }
 
 export default Car
-
